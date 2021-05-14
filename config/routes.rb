a0fd4c20102b8b51bb32113b8ddab515622c578e
Josesosa0777 <josephsosa0777@gmail.com>
2021-05-14 10:40:19 -0500

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  root to: "home#index"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/welcome', to: 'home#index'
+  get 'people/new', to: 'people#new'
+  post 'people', to: 'people#create'
+  get 'people/:id', to: 'people#show'
+  get 'people/:id/edit', to: 'people#edit'
+  patch 'people/:id', to: 'people#update', as: :person
+  delete 'people/:id', to: 'people#destroy'
+  get 'people', to: 'people#index'
+  get 'people/new', to: 'people#new', as: :new_people
+end
